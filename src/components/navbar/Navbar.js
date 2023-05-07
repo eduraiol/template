14579f7c28ff6d0382d09a3c67182c24462dfe33
Eduardo Raiol <marquesraiol@gmail.com>
2023-05-07 19:46:37 -0300

    template react

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,34 @@
+import "./Navbar.css";
+import avatar from "../../assets/avatar.jpg";
+
+const Navbar = ({ sidebarOpen, openSidebar}) => {
+    return(
+        <nav className="navbar">
+            <div className="nav_icon" onClick={() => openSidebar()}>
+                <i className="fa fa-bars" aria-hidden="true"></i>
+            </div>
+
+            <div className="navbar__left">
+                <a href="#">Produtos</a>
+                <a href="#">Usuários</a>
+                <a href="#" className="active_link">Admin</a>
+            </div>
+
+            <div className="navbar__right">
+                <a href="#">
+                    <i className="fa fa-search"></i>
+                </a>
+
+                <a href="#">
+                    <i className="fa fa-clock-o"></i>
+                </a>
+
+                <a href="#">
+                    <img width="30px" src={avatar} alt="avatar" ></img>
+                </a>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
